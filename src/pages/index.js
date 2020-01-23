b4e48975e5b15fb931c5bc8ac4e65e2345b39d7f
Conrad <15138480+creyD@users.noreply.github.com>
2020-01-23 15:32:46 +0100

    Added Paragraph to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,18 @@ export default () => (
           <li>Steam Community Group</li>
         </a>
       </ul>
+      <MainEntry title="Contribute" />
+      <p>
+        As it was requested by some people over the last years, we finally
+        created a{" "}
+        <a href="https://paypal.me/pools/c/89fz0b2YFk" target="_blanc">
+          donation link (PayPal).
+        </a>{" "}
+        All the donated money will go 100% towards keeping up the servers and
+        running other community related projects in the future. However our
+        services will continue to be freely available for anyone. -{" "}
+        <b>Thank you to everyone who donated.</b>
+      </p>
       <MainEntry title="Help" />
       <p>
         For help with any community related matters, please contact one of our
