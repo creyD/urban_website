03e22e7b40f4dea0d7929cb54cb1b4439ad3eea3
Conrad <grosserconrad@gmail.com>
2020-01-24 08:48:26 +0100

    Added Production Settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,18 @@
  */
 
 module.exports = {
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    name: `Urban Brothers Homepage`,
+    short_name: `Urban Website`,
+    start_url: `/`,
+    background_color: `#6b37bf`,
+    theme_color: `#6b37bf`,
+    // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    display: `standalone`,
+    icon: `src/images/icon.png`, // This path is relative to the root of the site.
+  },
   siteMetadata: {
     title: `Urban Brothers Website`,
     siteURL: `https://urbanbrothers.de`,
