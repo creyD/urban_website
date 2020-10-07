baa9d67d182016529b3aecb0f4852b000493f2ef
Conrad <grosserconrad@gmail.com>
2020-10-07 10:35:14 +0200

    Added django project

---
+++ src/ub/urls.py
@@ -0,0 +1,9 @@
+"""ub URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls)
+]
